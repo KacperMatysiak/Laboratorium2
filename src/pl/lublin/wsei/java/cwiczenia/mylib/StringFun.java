ee0d2d9e107b5070aa47e740495d591c50cfb629
KacperMatysiak <35163@studentwsei.pl>
2021-03-18 21:56:36 +0100

    lab 2 - zad 2 do punkt d

--- src/pl/lublin/wsei/java/cwiczenia/mylib/StringFun.java
+++ src/pl/lublin/wsei/java/cwiczenia/mylib/StringFun.java
@@ -10,15 +10,11 @@ public class StringFun {
         char[] otab = arg.toCharArray();
         int j = 0;
         for (int i = ctab.length - 1; i >= 0; i--, j++) {
-            System.out.println(otab[i] + " | " + ctab[j]);
             odwroc += otab[i];
         }
 
-        System.out.println(odwroc);
-        System.out.println(arg);
 
         boolean wynik = odwroc == arg;
-        System.out.println(wynik);
         return wynik;
     }
 }
