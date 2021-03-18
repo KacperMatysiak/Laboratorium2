80bdcaa26083b8958351dca83c18399709df622f
KacperMatysiak <35163@studentwsei.pl>
2021-03-18 21:53:41 +0100

    lab 2 - zad 2 do punkt d

--- src/pl/lublin/wsei/java/cwiczenia/mylib/StringFun.java
+++ src/pl/lublin/wsei/java/cwiczenia/mylib/StringFun.java
@@ -1,12 +1,24 @@
 package pl.lublin.wsei.java.cwiczenia.mylib;
 import pl.lublin.wsei.java.cwiczenia.isPalindromeTest;
 public class StringFun {
-    public static boolean isPalindrome(String word) {
-        return false;
+    public static boolean isPalindrome(String arg) {
 
-//    }
-//    public static String anarchize(String word) {
-//
-//    }
+
+        String odwroc = "";
+
+        char[] ctab = arg.toCharArray();
+        char[] otab = arg.toCharArray();
+        int j = 0;
+        for (int i = ctab.length - 1; i >= 0; i--, j++) {
+            System.out.println(otab[i] + " | " + ctab[j]);
+            odwroc += otab[i];
+        }
+
+        System.out.println(odwroc);
+        System.out.println(arg);
+
+        boolean wynik = odwroc == arg;
+        System.out.println(wynik);
+        return wynik;
     }
 }
