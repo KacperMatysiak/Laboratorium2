fab6cc51a3d4dc49836636adcb004b7b4e201cc4
KacperMatysiak <35163@studentwsei.pl>
2021-03-18 19:44:27 +0100

    Initial commit

---
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -0,0 +1,11 @@
+package pl.lublin.wsei.java.cwiczenia;
+
+import pl.lublin.wsei.java.cwiczenia.mylib.Account;
+
+public class Main {
+    public static void main(String[] args) {
+        Account acc = new Account();
+        acc.setName("piotr Gołabek");
+        System.out.println(acc.getName());
+    }
+}
